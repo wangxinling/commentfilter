51ac92d6d0ab4c3d2c66fc4d148d1d36867b78df
wangxinling <8239838@qq.com>
2021-08-08 02:43:01 +1000

    Add testing framework

--- routes/route.js
+++ routes/route.js
@@ -59,6 +59,16 @@ router.post('/logout', (req, res, next) => {
     })(req, res, next);
   });
 
+  router.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+    var firstNumber = parseInt(req.params.firstNumber) 
+    var secondNumber = parseInt(req.params.secondNumber)
+    var result = firstNumber + secondNumber || null
+    if(result == null) {
+      res.json({result: result, statusCode: 400}).status(400)
+    }
+    else { res.json({result: result, statusCode: 200}).status(200) } 
+  })
+
 module.exports = router;
 
 
